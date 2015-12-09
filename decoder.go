24c133edac574d395b831107d97ba8e38d25b434
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-12-09 10:32:39 +0100

    reader: remove one switch, document

--- decoder.go
+++ decoder.go
@@ -7,67 +7,80 @@ import (
 	"strings"
 )
 
-// Uevent represents a single uevent event.
+// Uevent represents a single uevent.
 type Uevent struct {
 	Header string
 
-	// default uevent keys according to kobject_uevent.c
+	// default uevent keys as per kobject_uevent.c
 	Action    string
 	Devpath   string
 	Subsystem string
 	Seqnum    string
 
-	// All keys of this uevent
+	// A key/value map of all keys
 	Keys map[string]string
 }
 
+// Decoder decodes uevents from a reader.
 type Decoder struct {
 	r *bufio.Reader
 }
 
+// NewDecoder creates an uevent decoder
+// using the given reader to read uevents from.
 func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{bufio.NewReader(r)}
 }
 
+// Decode blocks until the uext uevent happens, decodes and returns it.
+// It is meant to be used in a loop.
 func (d *Decoder) Decode() (*Uevent, error) {
 	ev := &Uevent{
 		Keys: map[string]string{},
 	}
 
+	h, err := d.next()
+	if err != nil {
+		return nil, err
+	}
+	ev.Header = h
+
 loop:
 	for {
-		s, err := d.r.ReadString(0x00)
+		kv, err := d.next()
 		if err != nil {
 			return nil, err
 		}
 
-		switch {
-		case strings.Contains(s, "@"):
-			ev.Header = s
-		case strings.Contains(s, "="):
-			kv := strings.Split(s, "=")
+		i := strings.Index(kv, "=")
+		if i < 0 {
+			return nil, errors.New("error decoding uevent: unknown format")
+		}
 
-			if len(kv) != 2 {
-				return nil, errors.New("error reading event: unknown format")
-			}
+		k, v := kv[:i], kv[i+1:]
+		ev.Keys[k] = v
 
-			k, v := kv[0], kv[1]
-
-			ev.Keys[k] = v
-
-			switch k {
-			case "ACTION":
-				ev.Action = v
-			case "DEVPATH":
-				ev.Devpath = v
-			case "SUBSYSTEM":
-				ev.Subsystem = v
-			case "SEQNUM":
-				ev.Seqnum = v
-				break loop
-			}
+		switch k {
+		case "ACTION":
+			ev.Action = v
+		case "DEVPATH":
+			ev.Devpath = v
+		case "SUBSYSTEM":
+			ev.Subsystem = v
+		case "SEQNUM": // implicitely signals a complete uevent
+			ev.Seqnum = v
+			break loop
 		}
 	}
 
 	return ev, nil
+}
+
+// next returns the next event token
+func (d *Decoder) next() (string, error) {
+	s, err := d.r.ReadString(0x00)
+	if err != nil {
+		return "", err
+	}
+	return s, nil
 }
