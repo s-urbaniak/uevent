54a399528d3547ff5eb569536bdae31834ecfd41
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2015-12-28 22:06:23 +0100

    decoder: remove last char (0) of uevent value

--- decoder.go
+++ decoder.go
@@ -57,7 +57,7 @@ loop:
 			return nil, errors.New("error decoding uevent: unknown format")
 		}
 
-		k, v := kv[:i], kv[i+1:]
+		k, v := kv[:i], kv[i+1:len(kv)-1] // last char is zero (0)
 		ev.Vars[k] = v
 
 		switch k {
